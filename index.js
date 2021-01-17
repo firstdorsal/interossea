189b9125f13d13aa4825c8ec57d433ae8ac9d3c5
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-01-18 00:27:48 +0100

    dont drop collection

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const { authenticator } = require("otplib");
 
 app.listen(process.env.PORT !== undefined ? process.env.PORT : 80);
 console.log("server started");
-if (process.env.DEVELOPMENT) db.get("login").drop();
+//if (process.env.DEVELOPMENT) db.get("login").drop();
 const webSchema = process.env.WEB_SCHEMA != undefined ? process.env.WEB_SCHEMA : "https";
 console.log("debug mode set to " + !!process.env.DEBUG);
 const D = process.env.DEBUG;
