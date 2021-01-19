68c9ff470832ee1a182b7a79fa22d6ab16dfa591
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-01-19 22:53:09 +0100

    debug

--- index.js
+++ index.js
@@ -33,8 +33,9 @@ app.use(helmet.hidePoweredBy());
 app.disable("etag");
 
 app.use((req, res, next) => {
+    console.log(req.headers);
+
     if (req.get(`Host`) === process.env.WEB_URI && req.get(`origin`) === webSchema + "://" + process.env.WEB_URI && req.get("Content-Type") === "application/json") {
-        console.log(req.headers);
         return next();
     }
     return res.sendStatus(400);
