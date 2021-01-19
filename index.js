0622b1a0b7ea4b5916345846333be06dac254b26
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-01-19 22:50:52 +0100

    add application type checking

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(helmet.hidePoweredBy());
 app.disable("etag");
 
 app.use((req, res, next) => {
-    if (req.get(`Host`) === process.env.WEB_URI) {
+    if (req.get(`Host`) === process.env.WEB_URI && req.get(`origin`) === webSchema + "://" + process.env.WEB_URI && req.get("Content-Type") === "application/json") {
         console.log(req.headers);
         return next();
     }
