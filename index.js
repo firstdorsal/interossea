b36e64d82791328c7e387a6dd2db3920a3a053e1
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-07-11 15:23:49 +0200

    add quotes arround column names

--- index.js
+++ index.js
@@ -28,44 +28,45 @@ const db = new Client({
     password: "password",
     database: "db"
 });
-db.connect();
+db.connect().catch(() => {});
 
 db.query(
     `
+    
     CREATE TABLE login (
-            email varchar(254) NOT NULL UNIQUE,
-            time bigint NOT NULL,
-            token varchar(100) NOT NULL,
-            ip varchar(50) NOT NULL,
-            preSessionId varchar(100) NOT NULL,
-            PRIMARY KEY (email)
-        );
+        "email" varchar(254) NOT NULL UNIQUE,
+        "time" bigint NOT NULL,
+        "token" varchar(100) NOT NULL,
+        "ip" varchar(50) NOT NULL,
+        "preSessionId" varchar(100) NOT NULL,
+        PRIMARY KEY (email)
+    );
     CREATE TABLE login2 (
-        firstFactorToken varchar(100) NOT NULL UNIQUE,
-        userId varchar(15) NOT NULL,
-        time bigint NOT NULL,
-        ip varchar(50) NOT NULL,
-        webAuthnLoginChallenge varchar(100),
-        PRIMARY KEY (firstFactorToken)
+        "firstFactorToken" varchar(100) NOT NULL UNIQUE,
+        "userId" varchar(15) NOT NULL,
+        "time" bigint NOT NULL,
+        "ip" varchar(50) NOT NULL,
+        "webAuthnLoginChallenge" varchar(100),
+        PRIMARY KEY ("firstFactorToken")
     );
     CREATE TABLE users (
-        email varchar(254) NOT NULL UNIQUE,
-        userId varchar(15) NOT NULL UNIQUE,
-        time bigint NOT NULL,
-        totpSecret varchar(100),
-        totpActive boolean DEFAULT false,
-        webAuthnRegisterChallenge varchar(100),
-        webAuthnActive boolean DEFAULT false,
-        webAuthnKey json,
-        PRIMARY KEY (userId)
+        "email" varchar(254) NOT NULL UNIQUE,
+        "userId" varchar(15) NOT NULL UNIQUE,
+        "time" bigint NOT NULL,
+        "totpSecret" varchar(100),
+        "totpActive" boolean DEFAULT false,
+        "webAuthnRegisterChallenge" varchar(100),
+        "webAuthnActive" boolean DEFAULT false,
+        "webAuthnKey" json,
+        PRIMARY KEY ("userId")
     );
     CREATE TABLE sessions (
-        sessionId varchar(100) NOT NULL UNIQUE,
-        userId varchar(15) NOT NULL,
-        time bigint NOT NULL,
-        ip varchar(50) NOT NULL,
-        PRIMARY KEY (sessionId)
-    );  
+        "sessionId" varchar(100) NOT NULL UNIQUE,
+        "userId" varchar(15) NOT NULL,
+        "time" bigint NOT NULL,
+        "ip" varchar(50) NOT NULL,
+        PRIMARY KEY ("sessionId")
+    );    
     `
 ).catch(() => {});
 
@@ -139,10 +140,10 @@ app.get(`${BASE_URL}/v1/createsession`, async (req, res) => {
     const t = xss(req.query.t);
 
     // find corresponding email for token t
-    const a = (await db.query(`SELECT * FROM login WHERE token=$1`, [t])).rows[0];
+    const a = (await db.query(`SELECT * FROM "login" WHERE "token"=$1`, [t])).rows[0];
 
     // delete login
-    await db.query(`DELETE FROM login WHERE token=$1`, [t]);
+    await db.query(`DELETE FROM "login" WHERE "token"=$1`, [t]);
 
     // check if token exists and hasnt expired
     const ip = req.ip;
@@ -156,23 +157,22 @@ app.get(`${BASE_URL}/v1/createsession`, async (req, res) => {
     }
 
     if (!req.cookies) return webResponse(res, { message: `missing cookies`, error: true });
+
     if (!req.cookies.preSessionId) return webResponse(res, { message: `missing preSessionId cookie: Request was sent from a different origin/browser`, error: true }, true);
 
-    const preSessionId = req.cookies.preSessionId;
-
-    // check if browser origin and device is the same
-    if (!a.preSessionId || a.preSessionId !== preSessionId) {
+    // check if last request was sent from same browser
+    if (!a.preSessionId || a.preSessionId !== req.cookies.preSessionId) {
         return webResponse(res, { message: `invalid preSessionId cookie: Request was sent from a different origin/browser`, error: true }, true);
     }
 
     // try to find user with email
-    let user = (await db.query(`SELECT * FROM users WHERE email=$1`, [a.email])).rows[0];
+    let user = (await db.query(`SELECT * FROM "users" WHERE "email"=$1`, [a.email])).rows[0];
 
     // create new user if dont exist
     if (!user) {
         const userId = `u${generateToken(14)}`;
 
-        await db.query(`INSERT INTO users (email, userId, time) VALUES ($1, $2, $3)`, [a.email, userId, Date.now()]);
+        await db.query(`INSERT INTO "users" ("email", "userId", "time") VALUES ($1, $2, $3)`, [a.email, userId, Date.now()]);
         user = { userId };
     } else if (user.totpActive || user.webAuthnActive) {
         // check if 2fa is present
@@ -180,7 +180,7 @@ app.get(`${BASE_URL}/v1/createsession`, async (req, res) => {
         const firstFactorToken = generateToken(100);
 
         // add firstFactorToken to db
-        await db.query(`INSERT INTO login2 (firstFactorToken, userId, time, ip) VALUES ($1, $2, $3, $4)`, [firstFactorToken, userId, Date.now(), ip]);
+        await db.query(`INSERT INTO "login2" ("firstFactorToken", "userId", "time", "ip") VALUES ($1, $2, $3, $4)`, [firstFactorToken, userId, Date.now(), ip]);
 
         // append firstFactorToken cookie to response
         res.cookie(`firstFactorToken`, firstFactorToken, {
@@ -196,7 +196,7 @@ app.get(`${BASE_URL}/v1/createsession`, async (req, res) => {
     const newSessionID = generateToken(100);
 
     // save session cookie to db
-    await db.query(`INSERT INTO sessions (sessionId, userId, time, ip) VALUES ($1, $2, $3, $4)`, [newSessionID, user.userId, Date.now(), ip]);
+    await db.query(`INSERT INTO "sessions" ("sessionId", "userId", "time", "ip") VALUES ($1, $2, $3, $4)`, [newSessionID, user.userId, Date.now(), ip]);
 
     // append session cookie to response
     res.cookie(`sessionId`, newSessionID, {
@@ -222,7 +222,7 @@ app.post(`${BASE_URL}/v1/login`, async (req, res) => {
         return webResponse(res, { message: `invalid mail`, error: true });
     }
     const email = xss(req.body.email);
-    const a = (await db.query(`SELECT * FROM login WHERE email=$1`, [email])).rows[0];
+    const a = (await db.query(`SELECT * FROM "login" WHERE "email"=$1`, [email])).rows[0];
 
     const token = generateToken(100);
     const preSessionId = generateToken(100);
@@ -241,9 +241,9 @@ app.post(`${BASE_URL}/v1/login`, async (req, res) => {
                 true
             );
         }
-        db.query("UPDATE login SET time=$2, token=$3, ip=$4, preSessionId=$5 WHERE email=$1", [email, Date.now(), token, ip, preSessionId]);
+        db.query(`UPDATE "login" SET time=$2, token=$3, ip=$4, "preSessionId"=$5 WHERE email=$1`, [email, Date.now(), token, ip, preSessionId]);
     } else {
-        db.query("INSERT INTO login (email,time,token,ip,preSessionId) VALUES ($1,$2,$3,$4,$5)", [email, Date.now(), token, ip, preSessionId]);
+        db.query(`INSERT INTO "login" ("email", "time", "token", "ip", "preSessionId") VALUES ($1,$2,$3,$4,$5)`, [email, Date.now(), token, ip, preSessionId]);
     }
 
     const transporter = nodemailer.createTransport({
@@ -300,7 +300,7 @@ app.post(`${BASE_URL}/v1/2fa/totp/generate`, async (req, res) => {
     }
     const secret = authenticator.generateSecret();
     const otpauth = authenticator.keyuri(a.userId, WEB_URL, secret);
-    db.query("UPDATE users SET totpSecret=$2 WHERE userId=$1", [user.userId, secret]);
+    db.query(`UPDATE "users" SET "totpSecret"=$2 WHERE "userId"=$1`, [user.userId, secret]);
 
     qrcode.toDataURL(otpauth, (err, url) => {
         res.status(200).send({
@@ -316,7 +316,7 @@ app.post(`${BASE_URL}/v1/2fa/totp/register`, async (req, res) => {
     if (!req.body) return webResponse(res, { message: `missing body`, error: true });
     if (!req.body.totp) return webResponse(res, { message: `totp token missing`, error: true });
     if (authenticator.generate(a.totpSecret) === req.body.totp) {
-        db.query("UPDATE users SET totpActive=true WHERE userId=$1", [user.userId]);
+        db.query(`UPDATE "users" SET "totpActive"=true WHERE "userId"=$1`, [user.userId]);
         return webResponse(res, { message: `correct totp token`, error: false }, true);
     }
     return webResponse(res, { message: `invalid totp token`, error: true }, true);
@@ -330,7 +330,7 @@ app.post(`${BASE_URL}/v1/2fa/webauthn/register/request`, async (req, res) => {
         relyingParty: { name: process.env.DISPLAY_NAME },
         user: { id: user.userId, name: user.userId }
     });
-    db.query("UPDATE users SET webAuthnRegisterChallenge=$2 WHERE userId=$1", [user.userId, challengeResponse.challenge]);
+    db.query(`UPDATE "users" SET "webAuthnRegisterChallenge"=$2 WHERE "userId"=$1`, [user.userId, challengeResponse.challenge]);
 
     return res.send(challengeResponse);
 });
@@ -342,7 +342,7 @@ app.post(`${BASE_URL}/v1/2fa/webauthn/register/verify`, async (req, res) => {
     const { key, challenge } = parseRegisterRequest(req.body);
 
     if (challenge === user.webAuthnRegisterChallenge) {
-        db.query("UPDATE users SET webAuthnKey=$2, webAuthnRegisterChallenge=null, webAuthnActive=true WHERE userId=$1", [user.userId, key]);
+        db.query(`UPDATE "users" SET "webAuthnKey"=$2, "webAuthnRegisterChallenge"=null, "webAuthnActive"=true WHERE "userId"=$1`, [user.userId, key]);
         return webResponse(res, { message: `Success`, error: false }, true);
     }
     return webResponse(res, { message: `WebAuthn challenge failed`, error: true }, true);
@@ -354,16 +354,16 @@ app.post(`${BASE_URL}/v1/2fa/createsession/totp`, async (req, res) => {
     if (!req.body) return webResponse(res, { message: `missing body`, error: true });
     if (!req.body.totp) return webResponse(res, { message: `missing totp object in body`, error: true });
 
-    const login = (await db.query("SELECT * FROM login2 WHERE firstFactorToken=$1", [req.cookies.firstFactorToken])).rows[0];
+    const login = (await db.query(`SELECT * FROM "login2" WHERE "firstFactorToken"=$1`, [req.cookies.firstFactorToken])).rows[0];
     if (!login) return webResponse(res, { message: `invalid firstFactorToken`, error: true });
 
-    const user = (await db.query("SELECT * FROM users WHERE userId=$1", [login.userId])).rows[0];
+    const user = (await db.query(`SELECT * FROM "users" WHERE "userId"=$1`, [login.userId])).rows[0];
     if (authenticator.generate(user.totpSecret) === req.body.totp) {
         //generate session id
         const newSessionID = generateToken(100);
 
         //save session cookie to db
-        await db.query(`INSERT INTO sessions (sessionId, userId, time, ip) VALUES ($1, $2, $3, $4)`, [newSessionID, user.userId, Date.now(), req.ip]);
+        await db.query(`INSERT INTO "sessions" ("sessionId", "userId", "time", "ip") VALUES ($1, $2, $3, $4)`, [newSessionID, user.userId, Date.now(), req.ip]);
 
         //append session cookie to response
         res.cookie(`sessionId`, newSessionID, {
@@ -384,16 +384,16 @@ app.post(`${BASE_URL}/v1/2fa/createsession/totp`, async (req, res) => {
 app.post(`${BASE_URL}/v1/2fa/createsession/webauthn/request`, async (req, res) => {
     if (!req.cookies) return webResponse(res, { message: `missing cookies`, error: true });
     if (!req.cookies.firstFactorToken) return webResponse(res, { message: `missing firstFactorToken cookie`, error: true });
-    const login = (await db.query("SELECT * FROM login2 WHERE firstFactorToken=$1", [req.cookies.firstFactorToken])).rows[0];
+    const login = (await db.query(`SELECT * FROM "login2" WHERE "firstFactorToken"=$1`, [req.cookies.firstFactorToken])).rows[0];
 
     if (!login) return webResponse(res, { message: `invalid firstFactorToken`, error: true });
 
-    const user = (await db.query("SELECT * FROM users WHERE userId=$1", [login.userId])).rows[0];
+    const user = (await db.query(`SELECT * FROM "users" WHERE "userId"=$1`, [login.userId])).rows[0];
     if (!user) return webResponse(res, { message: `user not found`, error: true });
     if (!user.webAuthnKey) return webResponse(res, { message: `missing public key for this user`, error: true });
 
     const newChallenge = generateLoginChallenge(user.webAuthnKey);
-    db.query("UPDATE login2 SET webAuthnLoginChallenge=$2 WHERE firstFactorToken=$1", [login.firstFactorToken, newChallenge.challenge]);
+    db.query(`UPDATE "login2" SET "webAuthnLoginChallenge"=$2 WHERE "firstFactorToken"=$1`, [login.firstFactorToken, newChallenge.challenge]);
 
     newChallenge.userVerification = `preferred`;
     res.send(newChallenge);
@@ -403,9 +403,9 @@ app.post(`${BASE_URL}/v1/2fa/createsession/webauthn/verify`, async (req, res) =>
     if (!req.cookies) return webResponse(res, { message: `missing cookies`, error: true });
     if (!req.cookies.firstFactorToken) return webResponse(res, { message: `missing firstFactorToken cookie`, error: true });
     if (!req.body) return webResponse(res, { message: `missing body`, error: true });
-    const login = (await db.query("SELECT * FROM login2 WHERE firstFactorToken=$1", [req.cookies.firstFactorToken])).rows[0];
+    const login = (await db.query(`SELECT * FROM "login2" WHERE "firstFactorToken"=$1`, [req.cookies.firstFactorToken])).rows[0];
     if (!login) return webResponse(res, { message: `invalid firstFactorToken`, error: true });
-    const user = (await db.query("SELECT * FROM users WHERE userId=$1", [login.userId])).rows[0];
+    const user = (await db.query(`SELECT * FROM "users" WHERE "userId"=$1`, [login.userId])).rows[0];
     if (!user) return webResponse(res, { message: `user not found`, error: true });
     if (!user.webAuthnKey) return webResponse(res, { message: `missing public key for this user`, error: true });
 
@@ -420,7 +420,7 @@ app.post(`${BASE_URL}/v1/2fa/createsession/webauthn/verify`, async (req, res) =>
         const newSessionID = generateToken(100);
 
         //save session cookie to db
-        await db.query(`INSERT INTO sessions (sessionId, userId, time, ip) VALUES ($1, $2, $3, $4)`, [newSessionID, user.userId, Date.now(), req.ip]);
+        await db.query(`INSERT INTO "sessions" ("sessionId", "userId, "time", "ip") VALUES ($1, $2, $3, $4)`, [newSessionID, user.userId, Date.now(), req.ip]);
 
         //append session cookie to response
         res.cookie(`sessionId`, newSessionID, {
@@ -439,7 +439,7 @@ app.post(`${BASE_URL}/v1/2fa/createsession/webauthn/verify`, async (req, res) =>
 
 app.post(`${BASE_URL}/v1/logout`, async (req, res) => {
     if (!req.cookies || !req.cookies.sessionId) return webResponse(res, { message: `missing cookies`, error: true });
-    await db.query("DELETE FROM sessions WHERE sessionId=$1", [req.cookies.sessionId]);
+    await db.query(`DELETE FROM "sessions" WHERE "sessionId"=$1`, [req.cookies.sessionId]);
     res.cookie(`sessionId`, ``, {
         maxAge: 1,
         ...SECURE_COOKIE_ATTRIBUTES
@@ -453,13 +453,13 @@ app.post(`${BASE_URL}/v1/delete-account`, async (req, res) => {
     const user = await checkSession(req);
     if (!user) return webResponse(res, { message: `invalid session`, error: true }, true);
 
-    await db.query("DELETE FROM sessions WHERE sessionId=$1", [req.cookies.sessionId]);
+    await db.query(`DELETE FROM "sessions" WHERE "sessionId"=$1`, [req.cookies.sessionId]);
 
     res.cookie(`sessionId`, ``, {
         maxAge: 1,
         ...SECURE_COOKIE_ATTRIBUTES
     });
-    await db.query("DELETE FROM users WHERE userId=$1", [user.userId]);
+    await db.query(`DELETE FROM "users" WHERE "userId"=$1`, [user.userId]);
     return res.sendStatus(200);
 });
 
@@ -472,9 +472,9 @@ app.post(`*`, (req, res) => {
 
 // check session token
 const checkSession = async req => {
-    const session = (await db.query("SELECT * FROM sessions WHERE sessionId=$1", [req.cookies.sessionId])).rows[0];
+    const session = (await db.query(`SELECT * FROM "sessions" WHERE "sessionId"=$1`, [req.cookies.sessionId])).rows[0];
     if (!session) return false;
-    const user = (await db.query("SELECT * FROM users WHERE userId=$1", [session.userId])).rows[0];
+    const user = (await db.query(`SELECT * FROM "users" WHERE "userId"=$1`, [session.userId])).rows[0];
     if (!user) return false;
     return user;
 };
